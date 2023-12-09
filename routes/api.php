4993d998fb6d7ccd0aaea47fc83158fecaa3c9af
Pirisinu <palazzialessandro2@gmail.com>
2023-12-09 21:08:32 +0100

    [ edit ] Route name: '/prova-api' -> '/project-api'

--- routes/api.php
+++ routes/api.php
@@ -15,4 +15,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/prova-api', [ProjectController::class, 'index'])->name('prova-api');
+Route::get('/project-api', [ProjectController::class, 'index'])->name('prova-api');
